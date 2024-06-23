fd37982bd15ab74acf7929df532df040ed6d267e
Muhammed-Owais01 <workandrecovery@gmail.com>
2024-06-23 23:29:45 +0500

    Creating an ecom api

---
+++ app.js
@@ -0,0 +1,29 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
